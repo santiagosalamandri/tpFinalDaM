70d5da7131e8861cfd8c379e7c2cdb0166e378aa
santiago salamandri <santiagosalamandri@gmail.com>
2021-08-23 22:12:18 +0100

    Added GET Dispositivo by id

--- backend/routes/dispositivo/index.js
+++ backend/routes/dispositivo/index.js
@@ -13,4 +13,14 @@ routerDispositivo.get('/', function(req, res) {
     });
 });
 
+routerDispositivo.get('/:idDispositivo', function(req, res) {
+    pool.query('Select * from Dispositivos where dispositivoId=?', [req.params.idDispositivo], function(err, result, fields) {
+        if (err) {
+            res.send(err).status(400);
+            return;
+        }
+        res.send(result[0]);
+    });
+});
+
 module.exports = routerDispositivo;
