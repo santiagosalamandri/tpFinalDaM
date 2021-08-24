8909b9e71da29bfc25f170ba177ccbcaeb222944
santiago salamandri <santiagosalamandri@gmail.com>
2021-08-24 20:45:10 +0100

    Added chart for readings.

--- frontend/src/app/dispositivo/dispositivo.page.ts
+++ frontend/src/app/dispositivo/dispositivo.page.ts
@@ -4,6 +4,11 @@ import { MedicionService } from '../services/medicion.service';
 import * as moment from 'moment';
 import { Medicion } from '../model/Medicion';
 
+import * as Highcharts from 'highcharts';
+declare var require: any;
+require('highcharts/highcharts-more')(Highcharts);
+require('highcharts/modules/solid-gauge')(Highcharts);
+
 @Component({
   selector: 'app-dispositivo',
   templateUrl: './dispositivo.page.html',
@@ -13,8 +18,25 @@ export class DispositivoPage implements OnInit {
 
   public medicion:Medicion;
   public idDispositivo:string;
-  constructor(private router:ActivatedRoute, private mServ:MedicionService) { }
+  private valorObtenido:number=0;
+  public myChart;
+  private chartOptions;
 
+  constructor(private router:ActivatedRoute, private mServ:MedicionService) { 
+    setTimeout(()=>{
+      console.log("Cambio el valor del sensor.");
+      this.valorObtenido=this.medicion.valor-0;
+      //llamo al update del chart para refrescar y mostrar el nuevo valor
+      this.myChart.update({series: [{
+          name: 'kPA',
+          data: [this.valorObtenido],
+          tooltip: {
+              valueSuffix: ' kPA'
+          }
+      }]});
+    },1000);
+  }
+  
   ngOnInit() {
 
     this.idDispositivo = this.router.snapshot.paramMap.get('id');
@@ -34,7 +56,7 @@ export class DispositivoPage implements OnInit {
     //opción 2, utilizar el objeto Date y hacer el formato necesario a mano.
     let current_datetime = new Date()
     let formatted_date = current_datetime.getFullYear() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() + " " + current_datetime.getHours() + ":" + current_datetime.getMinutes() + ":" + current_datetime.getSeconds() 
-    let a : Medicion= new Medicion(99,formatted_date,99,1);
+    let a : Medicion= new Medicion(99,formatted_date,current_datetime.getSeconds(),1);
    
     this.mServ.agregarMedicion(a).then((med)=>{
       console.log(med)
@@ -42,7 +64,80 @@ export class DispositivoPage implements OnInit {
     
   }
 
+  ionViewDidEnter() {
+    this.generarChart();
+  }
 
+  generarChart() {
+    this.chartOptions={
+      chart: {
+          type: 'gauge',
+          plotBackgroundColor: null,
+          plotBackgroundImage: null,
+          plotBorderWidth: 0,
+          plotShadow: false
+        }
+        ,title: {
+          text: `Sensor N° `+this.idDispositivo
+        }
+
+        ,credits:{enabled:false}
+        
+           
+        ,pane: {
+            startAngle: -150,
+            endAngle: 150
+        } 
+        // the value axis
+      ,yAxis: {
+        min: 0,
+        max: 100,
+  
+        minorTickInterval: 'auto',
+        minorTickWidth: 1,
+        minorTickLength: 10,
+        minorTickPosition: 'inside',
+        minorTickColor: '#666',
+  
+        tickPixelInterval: 30,
+        tickWidth: 2,
+        tickPosition: 'inside',
+        tickLength: 10,
+        tickColor: '#666',
+        labels: {
+            step: 2,
+            rotation: 'auto'
+        },
+        title: {
+            text: 'kPA'
+        },
+        plotBands: [{
+            from: 0,
+            to: 10,
+            color: '#55BF3B' // green
+        }, {
+            from: 10,
+            to: 30,
+            color: '#DDDF0D' // yellow
+        }, {
+            from: 30,
+            to: 100,
+            color: '#DF5353' // red
+        }]
+    }
+    ,
+  
+    series: [{
+        name: 'kPA',
+        data: [this.valorObtenido],
+        tooltip: {
+            valueSuffix: ' kPA'
+        }
+    }]
+
+    };
+    this.myChart = Highcharts.chart('highcharts', this.chartOptions );
+  }
 
 
 }
